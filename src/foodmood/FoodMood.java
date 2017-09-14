5594691c241e845b25c5de956b3d8d17ca5c313a
Michael Kramer <mpk5206 @ psu.edu>
2017-09-14 16:55:46 -0400

    Initial Commit

---
+++ src/foodmood/FoodMood.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodmood;
+
+/**
+ *
+ * @author Michael Kramer <mpk5206 @ psu.edu>
+ */
+public class FoodMood {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
