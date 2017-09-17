a4f8592820d2187ff6910c072d1b8b02a224437f
Michael Kramer <mpk5206 @ psu.edu>
2017-09-17 15:55:54 -0400

    Changed Documentation thing.

--- src/foodmood/FoodMood.java
+++ src/foodmood/FoodMood.java
@@ -15,7 +15,8 @@ public class FoodMood {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+            System.err.println("This is a stub.");
+            //TODO: Implement main
 	}
 	
 }
