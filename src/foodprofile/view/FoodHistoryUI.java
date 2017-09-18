ddc44cb256bdff252695df6151bf552df43f9309
Michael Kramer <mpk5206@psu.edu>
2017-09-17 20:52:00 -0400

    Added Stubs

---
+++ src/foodprofile/view/FoodHistoryUI.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodprofile.view;
+
+/**
+ *
+ * @author Michael Kramer <mpk5206 @ psu.edu>
+ */
+public class FoodHistoryUI {
+	
+	/**
+	 * The Default Constructor for the FoodHistoryUI
+	 */
+	public FoodHistoryUI () {
+		
+	}
+}
