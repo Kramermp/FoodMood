ac26fbdee944a09ef5ca725465e816d76b6a5734
hkgarthwaite <hkgarthwaite@gmail.com>
2017-09-28 15:27:50 -0400

    Implemented Stubs

--- src/Login/LoginView.java
+++ src/Login/LoginView.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package login;
+package Login;
 
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
@@ -13,7 +13,7 @@ import javax.swing.JTextField;
  *
  * @author Michael Kramer
  */
-public class LoginView {
+public class LoginView{
     private LoginController parentController;
     private JTextField usernameTextField;
     private JPasswordField passwordField;
@@ -25,17 +25,33 @@ public class LoginView {
      * @param parentController the parentController
      */
     public LoginView(LoginController parentController) {
-        System.err.println("This is a stub.");
-        //TODO: Implment LoginView
+        this.parentController = parentController;
+    }
+    
+    private void initializeComponents(){
+        usernameTextField = new JTextField();
+        passwordField = new JPasswordField();
+        
+        loginButton = new JButton("Login");
+        loginButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                submitUserCredentials();
+            }   
+        });
+        
+        exitButton = new JButton("Exit");
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exit();
+            }   
+        });
     }
     
     private void exit() {
-        System.err.println("This is a stub.");
-        //TODO: Implment exit
+        System.exit(0);
     }
     
     private void submitUserCredentials() {
-        System.err.println("This is a stub.");
-        //TODO: Implment submitUserCredentials
+        parentController.authenticateUserCredentials(usernameTextField.getText(), passwordField.getPassword());
     }
 }
