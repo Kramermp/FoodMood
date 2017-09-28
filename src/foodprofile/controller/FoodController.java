642d3f8eba8b4a419d16977673719d1f5151eb0d
hilarygodin <hilarygodin@104.39.59.109>
2017-09-28 17:17:26 -0400

    Separated FoodMoodController to FoodController

--- src/foodprofile/controller/FoodController.java
+++ src/foodprofile/controller/FoodController.java
@@ -5,12 +5,60 @@
  */
 package foodprofile.controller;
 
+import foodprofile.model.FoodList;
+import foodprofile.model.FoodProfileModel;
+import userprofile.model.User;
+
 /**
  *
  * @author Michael Kramer
  */
 public class FoodController {
-    public FoodController () {
+
+    private FoodList foodList;
+        
+    public FoodController(User currentUser){
         
     }
+        
+     /**
+     * This returns the FoodList for the current user
+     * @return the foodList
+     */
+    public FoodList getFoodList() {
+        return foodList;
+    }
+
+    /**
+     * This sets the FoodList for the current user
+     * @param foodList the foodList to set
+     */
+    public void setFoodList(FoodList foodList) {
+        this.foodList = foodList;
+    }
+    
+    /**
+     * Adds a Food to the current user's FoodList
+     * @param food the food to add
+     */
+    public void addFood(FoodProfileModel food){
+        
+    }
+    
+    /**
+     * This updates a food, using the id stored within the food class
+     * @param food  the food to update
+     */
+    public void updateFood(FoodProfileModel food){
+        
+    }
+    
+    /**
+     * This deletes the food from the current user's FoodList
+     * @param foodID the food to delete
+     */
+    public void deleteFood(int foodID){
+        
+    }
+    
 }
