91c884c6d16cc8da9b365956b747ce0e4663ed8a
hkgarthwaite <hkgarthwaite@gmail.com>
2017-09-28 16:50:39 -0400

    Implemented some stubs

---
+++ src/test/FoodTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+import foodprofile.controller.*;
+import foodprofile.model.*;
+import foodprofile.view.*;
+/**
+ *
+ * @author HannahGarthwaite
+ */
+public class FoodTest {
+    
+    FoodController theFoodController;
+    
+    public FoodTest(FoodController theFoodController){
+        this.theFoodController = theFoodController;
+    }
+}
