6455dbfc10ce833937babfa5f5ed6aab6fecfaec
hkgarthwaite <hkgarthwaite@gmail.com>
2017-10-24 18:34:48 -0400

    Implented Iterator

--- src/notificationprofile/view/NotificationUI.java
+++ src/notificationprofile/view/NotificationUI.java
@@ -181,10 +181,12 @@ public class NotificationUI extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
         notificationCntl.next(notification.getId());
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.setVisible(false);
         notificationCntl.previous(notification.getId());
     }//GEN-LAST:event_jButton2ActionPerformed
 
