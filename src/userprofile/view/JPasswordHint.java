0afed95ba9792099194506b49b4f6340d22e6ed7
Michael Kramer <mpk5206@psu.edu>
2017-10-26 22:17:10 -0400

    Added Some documentation

--- src/userprofile/view/JPasswordHint.java
+++ src/userprofile/view/JPasswordHint.java
@@ -20,7 +20,7 @@ public class JPasswordHint extends JPasswordField {
     private Color textColor = Color.BLACK;
     private char echoChar;
     private char[] hint;
-    private char[] value;
+    private char[] value = {};
     
     public JPasswordHint (String hint, int width) {
         super(width);
@@ -30,31 +30,68 @@ public class JPasswordHint extends JPasswordField {
         this.addFocusListener(new PasswordFieldListener());
     }
     
+    /**
+     * Creates a JPassword field to display text before user inputs values
+     * @param hint The String to be displayed
+     */
+    public JPasswordHint (String hint) {
+        this.hint = hint.toCharArray();
+        this.echoChar = getEchoChar(); //Stores the default Echo char
+        displayHint();
+        this.addFocusListener(new PasswordFieldListener());
+    }
+    
+    /**
+     * Sets the text to be displayed
+     * @param text the text to be displayed
+     */
     public void setText(char[] text) {
         setText(String.valueOf(hint));
     }
     
+    /**
+     * This is used internally to display the hint to ensure that the class
+     * consistently makes the same changes
+     */
     private void displayHint() {
         setText(hint);
         setEchoChar((char) 0);
         setForeground(hintColor);
     }
     
+    /**
+     * This is used for when the User gets focus on the field to ensure it
+     * is empty in preparation for them entering their own data.
+     */
     private void clear() {
         setText("");
         setForeground(textColor);
         setEchoChar(echoChar);
     }
     
+    /**
+     * Returns the actual value of the field ignoring the hint
+     * @return the actual value
+     */
+    public char[] getValue() {
+        return value;
+    }
+    
+    /***
+     * This class is used to track when the PasswordField is actually updated
+     * and to ensure valid info.
+     */
     private class PasswordFieldListener implements FocusListener {
 
         @Override
         public void focusGained(FocusEvent fe) {
             System.out.println("Password Field gained Focus");
-            if(value == null) {
+            if(value == null || value.length == 0) {
+                //If it gains focus and there is no value
                 clear();
             } else {
-                
+                //If it gains focus and there is actual data in the field
+                //Do Nothing
             }
         }
 
@@ -62,15 +99,15 @@ public class JPasswordHint extends JPasswordField {
         public void focusLost(FocusEvent fe) {
             System.out.println("Password Field Lost Focus");
             if (getPassword().length == 0) {
+                //If it loses focus and there is no data
                 displayHint();
-                value = null;
+                value = new char[0]; //Clear old value
             } else {
+                //If it loses focus and there is values is there
                 value = getPassword();
                 setForeground(textColor);
             }
         }
-        
-        
-        
     }
+    
 }
--- src/userprofile/view/JTextHint.java
+++ src/userprofile/view/JTextHint.java
@@ -15,51 +15,108 @@ import javax.swing.JTextField;
  * @author Michael Kramer <mpk5206 @ psu.edu>
  */
 public class JTextHint extends JTextField {
+        private HintListener listener;
 	private Color textColor = Color.BLACK;
 	private Color hintColor = Color.GRAY;
 	private String hint = "";
 	private String value = "";
 	
+        /**
+         * Creates a JTextField that displays a text hint there is no value
+         * @param hint The String to display as a hint
+         * @param width The width of the field
+         */
+        public JTextHint(String hint, int width) {
+            super(width);
+            this.hint = hint;
+            this.listener = new HintListener();
+            this.addFocusListener(listener);
+            this.setForeground(hintColor);
+            this.setText(hint);
+	}
+        
+        /**
+         * Creates a JTextField that displays a text hint if there is no value
+         * @param hint The String to display as the hint
+         */
+        public JTextHint(String hint) {
+            this.hint = hint;
+            this.addFocusListener(new HintListener());
+            this.setForeground(hintColor);
+            this.setText(hint);
+        }
+        
+        /**
+         * Sets the color of the text after values have been input
+         * @param textColor The color to be displayed
+         */
 	public void setTextColor(Color textColor) {
             this.textColor = textColor;
 	}
 	
+        /**
+         * Sets the color of the hint
+         * @param hintColor 
+         */
 	public void setHintColor(Color hintColor) {
 		this.hintColor = hintColor;
 	}
 	
-	public JTextHint(String hint, int width) {
-            super(width);
-		this.hint = hint;
-		this.addFocusListener(new HintListener());
-		this.setForeground(hintColor);
-		this.setText(hint);
-	}
+        /** 
+         * Gets the actual value of the field ignoring the hint
+         * @return the value of the hint
+         */
+        public String getValue() {
+            return value;
+        }
+        
+        /**
+         * Used to empty the field when focus is gained.
+         */
+        private void clear () {
+            setText("");
+            setForeground(JTextHint.this.textColor);
+        }
+        public void prepareInput() {
+            if(value.isEmpty()) {
+                System.out.println("There is no value to the JTextHint");
+                clear();
+            } else {
+                //If there is a value
+                //Do Nothing
+                System.out.println("The value is " + value);  
+            }  
+        }
+        public void validateInput() {
+            if(getText() == hint) {
+                System.out.println("bvomm");
+            }
+            if(getText().isEmpty()) {
+                System.out.println("Input is empty");
+                setText(hint);
+                setForeground(hintColor);
+                value = "";
+            } else {
+                System.out.println("WARNIGNGNGNNGGNNG");
+                value = getText();
+            }
+        }
 	
+        /**
+         * This class is used to detect when the value of the field has changed
+         */
 	private class HintListener implements FocusListener {
 
-		@Override
-		public void focusGained(FocusEvent fe) {
-			System.out.println("Focus was gained");
-			setText(value);
-			setForeground(JTextHint.this.textColor);
-		}
+            @Override
+            public void focusGained(FocusEvent fe) {
+                System.out.println("JTextHint gained Focus");
+                prepareInput();
+            }
 
-		@Override
-		public void focusLost(FocusEvent fe) {
-			System.out.println("Focus Lost");
-			if(getText() == hint) {
-				//Do Nothing
-				System.out.println("The Value was not the hint");
-			} else {
-				if(getText().isEmpty()) {
-					setText(hint);
-					setForeground(hintColor);
-					value = "";
-				} else {
-					value = getText();
-				}
-			}
-		}
+            @Override
+            public void focusLost(FocusEvent fe) {
+                System.out.println("JTextHint Focus Lost");
+                validateInput();
+            }
 	}
 }
