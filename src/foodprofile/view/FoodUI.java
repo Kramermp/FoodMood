9b45997aaf051405cfd2bdfa33710c1383a74b33
hkgarthwaite <hkgarthwaite@gmail.com>
2017-10-29 21:32:57 -0400

    Added Read

--- src/foodprofile/view/FoodUI.java
+++ src/foodprofile/view/FoodUI.java
@@ -36,6 +36,8 @@ public class FoodUI extends JFrame{
     private JButton submit;
     private JComboBox monthsBox;
     private JComboBox daysBox;
+    private JButton listButton;
+    private JButton homeButton;
 
     
     /**
@@ -44,14 +46,14 @@ public class FoodUI extends JFrame{
      */
    
     public FoodUI(FoodCntl foodController) {
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         this.parentCntl = foodController;
         initializeComponents();
         
     }
     
     private void initializeComponents() {
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(400,400); 
         frame.setLayout(null);
         
@@ -115,6 +117,10 @@ public class FoodUI extends JFrame{
         submit.setBounds(150, 250, 80, 25);
         frame.add(submit);
         
+        listButton = new JButton("Food List");
+        listButton.setBounds(300, 250, 80, 25);
+        frame.add(listButton);
+        
         frame.setVisible(true);
         
 //        dateInput.addActionListener((ActionEvent ae) -> { 
@@ -137,6 +143,10 @@ public class FoodUI extends JFrame{
             GregorianCalendar time = new GregorianCalendar(2017, month, day);
             parentCntl.addFood(name, category, time);
 	});
+        listButton.addActionListener((ActionEvent ae) -> { 
+            this.setVisible(false);
+            parentCntl.goListView();
+	});
         
     }
       
@@ -149,10 +159,117 @@ public class FoodUI extends JFrame{
      * @param foodProfile the source foodprofile
 	 * @param parentController the parent controller
      */
-    public FoodUI(FoodCntl parentController, 
-            FoodProfileModel foodProfile) {
+    public FoodUI(FoodCntl parentController, Food food) {
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        this.parentCntl = parentController;
+        this.theFoodModel = food;
+        initView();
         
         
+    }
+    
+    public void initView(){
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(400,400); 
+        frame.setLayout(null);
+        
+        date = new JLabel("Date:");
+        date.setBounds(100, 75, 80, 25);
+        frame.add(date);
+        
+        //The text field option for date
+//        dateInput = new JTextField(50);
+//        dateInput.setBounds(200, 75, 80, 25);
+//        frame.add(dateInput);
+        
+        //Makes the date drop downs to elliminate user entering error
+        String[] months = new String[13];
+        for (int i = 1; i < 12; i++) {
+            months[i] = (i)+"";
+        }
+        months[0] = theFoodModel.getTime().getTime().getMonth()+"";
+        String[] days = new String[32];
+        for (int i = 1; i < 31; i++) {
+            days[i] = (i)+"";
+        }
+        days[0] = theFoodModel.getTime().getTime().getDate()+"";
+        JPanel dateInputPanel = new JPanel();
+        dateInputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        monthsBox = new JComboBox(months);
+        dateInputPanel.add(monthsBox);
+        JLabel slash = new JLabel("/");
+        dateInputPanel.add(slash);
+        daysBox = new JComboBox(days);
+        dateInputPanel.add(daysBox);
+        dateInputPanel.setBounds(200, 75, 180, 25);
+        frame.add(dateInputPanel);
+        
+        
+        time = new JLabel("Time:");
+        time.setBounds(100, 100, 80, 25);
+        frame.add(time);
+        
+        timeInput = new JTextField(50);
+        timeInput.setBounds(200, 100, 80, 25);
+        frame.add(timeInput);
+        
+        foodGroup = new JLabel("Food Group:");
+        foodGroup.setBounds(100, 125, 80, 25);
+        frame.add(foodGroup);
+        
+        String[] foodGroupOptions = {theFoodModel.getFoodCategories().get(0), "Dairy", "Fruits", "Grains", "Protein", "Vegetables"};
+        foodGroupInput = new JComboBox(foodGroupOptions);
+        foodGroupInput.setBounds(200, 125, 100, 25);
+        frame.add(foodGroupInput);
+        
+        foodName = new JLabel("Food:");
+        
+        foodName.setBounds(100, 150, 80, 25);
+        frame.add(foodName);
+        
+        foodInput = new JTextField(50);
+        foodInput.setText(theFoodModel.getName());
+        foodInput.setBounds(200, 150, 100, 50);
+        frame.add(foodInput);
+        
+        
+        submit = new JButton("Update");
+        submit.setBounds(150, 250, 80, 25);
+        frame.add(submit);
+        
+        listButton = new JButton("Food List");
+        listButton.setBounds(300, 250, 80, 25);
+        frame.add(listButton);
+        
+        frame.setVisible(true);
+        
+//        dateInput.addActionListener((ActionEvent ae) -> { 
+//            System.out.println("DateInput event triggered.");
+//	});
+        timeInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("TimeInput event triggered.");
+	});
+        foodGroupInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("foodGroupInput event triggered.");
+	});
+        foodInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("foodInput event triggered.");
+	});
+        submit.addActionListener((ActionEvent ae) -> { 
+            String name = foodInput.getText();
+            String category = foodGroupInput.getSelectedItem().toString();
+            int month = Integer.parseInt(monthsBox.getSelectedItem().toString());
+            int day = Integer.parseInt(daysBox.getSelectedItem().toString());
+            GregorianCalendar time = new GregorianCalendar(2017, month, day);
+            theFoodModel.setFoodCategory(category);
+            theFoodModel.setName(name);
+            theFoodModel.setTime(time);
+            parentCntl.updateFood(theFoodModel);
+	});
+        listButton.addActionListener((ActionEvent ae) -> { 
+            this.setVisible(false);
+            parentCntl.goListView();
+	});
     }
     
 }
