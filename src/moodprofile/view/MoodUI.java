1b3fe8fed06e7f4db73d852a57b8f0b12925d1f8
hkgarthwaite <hkgarthwaite@gmail.com>
2017-10-29 23:16:08 -0400

    Exception catching

--- src/moodprofile/view/MoodUI.java
+++ src/moodprofile/view/MoodUI.java
@@ -5,35 +5,56 @@
  */
 package moodprofile.view;
 
-import javax.swing.*;
+import foodmood.controller.NavigationCntl;
 import moodprofile.controller.MoodController;
 import moodprofile.model.Mood;
+import moodprofile.model.MoodProfileModel;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import javax.swing.*;
 
 /**
  *
- * @author Micahel Kramer
+ * @author mpk5206
  */
-public class MoodUI {
+public class MoodUI extends JFrame{
     private Mood theMoodModel;
     
-    private MoodController parentCntl;
     private JFrame frame = new JFrame("Mood");
+    private MoodController parentCntl;
     private JLabel moodName;
     private JTextField moodInput;
+    private JLabel moodGroup;
+    private JComboBox moodGroupInput;
     private JLabel date;
     private JTextField dateInput;
     private JLabel time;
     private JTextField timeInput;
     private JButton submit;
+    private JComboBox monthsBox;
+    private JComboBox daysBox;
+    private JButton listButton;
+    private JButton homeButton;
+    private JButton deleteButton;
+
+    
     /**
-     * Default constructor for MoodView
+     * Default Constructor for empty MoodView
+	 * @param parentController the parentController
      */
+   
     public MoodUI(MoodController moodController) {
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         this.parentCntl = moodController;
         initializeComponents();
+        
     }
-
+    
     private void initializeComponents() {
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(400,400); 
         frame.setLayout(null);
         
@@ -41,9 +62,32 @@ public class MoodUI {
         date.setBounds(100, 75, 80, 25);
         frame.add(date);
         
-        dateInput = new JTextField(50);
-        dateInput.setBounds(200, 75, 80, 25);
-        frame.add(dateInput);
+        //The text field option for date
+//        dateInput = new JTextField(50);
+//        dateInput.setBounds(200, 75, 80, 25);
+//        frame.add(dateInput);
+        
+        //Makes the date drop downs to elliminate user entering error
+        String[] months = new String[12];
+        for (int i = 0; i < 12; i++) {
+            months[i] = (i+1)+"";
+        }
+        String[] days = new String[31];
+        for (int i = 0; i < 31; i++) {
+            days[i] = (i+1)+"";
+        }
+        
+        JPanel dateInputPanel = new JPanel();
+        dateInputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        monthsBox = new JComboBox(months);
+        dateInputPanel.add(monthsBox);
+        JLabel slash = new JLabel("/");
+        dateInputPanel.add(slash);
+        daysBox = new JComboBox(days);
+        dateInputPanel.add(daysBox);
+        dateInputPanel.setBounds(200, 75, 180, 25);
+        frame.add(dateInputPanel);
+        
         
         time = new JLabel("Time:");
         time.setBounds(100, 100, 80, 25);
@@ -52,8 +96,9 @@ public class MoodUI {
         timeInput = new JTextField(50);
         timeInput.setBounds(200, 100, 80, 25);
         frame.add(timeInput);
+
         
-        moodName = new JLabel("Food:");
+        moodName = new JLabel("Mood:");
         moodName.setBounds(100, 150, 80, 25);
         frame.add(moodName);
         
@@ -65,6 +110,155 @@ public class MoodUI {
         submit.setBounds(150, 250, 80, 25);
         frame.add(submit);
         
+        listButton = new JButton("Mood List");
+        listButton.setBounds(300, 250, 180, 25);
+        frame.add(listButton);
+        
         frame.setVisible(true);
+        
+//        dateInput.addActionListener((ActionEvent ae) -> { 
+//            System.out.println("DateInput event triggered.");
+//	});
+        timeInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("TimeInput event triggered.");
+	});
+        moodInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("moodInput event triggered.");
+	});
+        submit.addActionListener((ActionEvent ae) -> { 
+            String name = moodInput.getText();
+            int month = Integer.parseInt(monthsBox.getSelectedItem().toString());
+            int day = Integer.parseInt(daysBox.getSelectedItem().toString());
+            GregorianCalendar time = new GregorianCalendar(2017, month, day);
+            parentCntl.addMood(name, time);
+	});
+        listButton.addActionListener((ActionEvent ae) -> { 
+            this.setVisible(false);
+            parentCntl.goListView();
+	});
+        
+        
     }
+      
+    
+    private void exit() {
+        
+    }
+    /**
+     * Creates a MoodView of the provided MoodProfile
+     * @param moodProfile the source moodprofile
+	 * @param parentController the parent controller
+     */
+    public MoodUI(MoodController parentController, Mood mood) {
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        this.parentCntl = parentController;
+        this.theMoodModel = mood;
+        initView();
+        
+        
+    }
+    
+    public void initView(){
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(400,400); 
+        frame.setLayout(null);
+        
+        date = new JLabel("Date:");
+        date.setBounds(100, 75, 80, 25);
+        frame.add(date);
+        
+        //The text field option for date
+//        dateInput = new JTextField(50);
+//        dateInput.setBounds(200, 75, 80, 25);
+//        frame.add(dateInput);
+        
+        //Makes the date drop downs to elliminate user entering error
+        String[] months = new String[13];
+        for (int i = 1; i < 12; i++) {
+            months[i] = (i)+"";
+        }
+        months[0] = theMoodModel.getTime().getTime().getMonth()+"";
+        String[] days = new String[32];
+        for (int i = 1; i < 31; i++) {
+            days[i] = (i)+"";
+        }
+        days[0] = theMoodModel.getTime().getTime().getDate()+"";
+        JPanel dateInputPanel = new JPanel();
+        dateInputPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+        monthsBox = new JComboBox(months);
+        dateInputPanel.add(monthsBox);
+        JLabel slash = new JLabel("/");
+        dateInputPanel.add(slash);
+        daysBox = new JComboBox(days);
+        dateInputPanel.add(daysBox);
+        dateInputPanel.setBounds(200, 75, 180, 25);
+        frame.add(dateInputPanel);
+        
+        
+        time = new JLabel("Time:");
+        time.setBounds(100, 100, 80, 25);
+        frame.add(time);
+        
+        timeInput = new JTextField(50);
+        timeInput.setBounds(200, 100, 80, 25);
+        frame.add(timeInput);
+        
+        moodName = new JLabel("Mood:");
+        
+        moodName.setBounds(100, 150, 80, 25);
+        frame.add(moodName);
+        
+        moodInput = new JTextField(50);
+        moodInput.setText(theMoodModel.getName());
+        moodInput.setBounds(200, 150, 100, 50);
+        frame.add(moodInput);
+        
+        
+        submit = new JButton("Update");
+        submit.setBounds(150, 250, 80, 25);
+        frame.add(submit);
+        
+        listButton = new JButton("Mood List");
+        listButton.setBounds(300, 250, 180, 25);
+        frame.add(listButton);
+        
+        deleteButton = new JButton("Delete");
+        deleteButton.setBounds(50, 250, 80, 25);
+        frame.add(deleteButton);
+        
+        frame.setVisible(true);
+        
+//        dateInput.addActionListener((ActionEvent ae) -> { 
+//            System.out.println("DateInput event triggered.");
+//	});
+        timeInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("TimeInput event triggered.");
+	});
+        moodGroupInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("moodGroupInput event triggered.");
+	});
+        moodInput.addActionListener((ActionEvent ae) -> { 
+            System.out.println("moodInput event triggered.");
+	});
+        submit.addActionListener((ActionEvent ae) -> { 
+            String name = moodInput.getText();
+            String category = moodGroupInput.getSelectedItem().toString();
+            int month = Integer.parseInt(monthsBox.getSelectedItem().toString());
+            int day = Integer.parseInt(daysBox.getSelectedItem().toString());
+            GregorianCalendar time = new GregorianCalendar(2017, month, day);
+            theMoodModel.setName(name);
+            theMoodModel.setTime(time);
+            parentCntl.updateMood(theMoodModel);
+	});
+        listButton.addActionListener((ActionEvent ae) -> { 
+            this.setVisible(false);
+            parentCntl.goListView();
+	});
+        deleteButton.addActionListener((ActionEvent ae) -> { 
+            this.setVisible(false);
+            parentCntl.deleteMood(theMoodModel);
+            parentCntl.goListView();
+	});
+    }
+    
 }
