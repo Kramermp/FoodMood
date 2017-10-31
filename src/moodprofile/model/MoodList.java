c53a02a5f062860e13a84eb92f0790f4db107612
hkgarthwaite <hkgarthwaite@gmail.com>
2017-10-31 12:11:35 -0400

    Moved CRUD to ExternalDataCntl

--- src/moodprofile/model/MoodList.java
+++ src/moodprofile/model/MoodList.java
@@ -22,25 +22,7 @@ public class MoodList {
     public MoodList(){
         System.out.println("New Mood List");
         listOfMoods = new ArrayList();
-        Statement theStatement = null;
-        Connection theConnection = null;
-        System.out.println("creating mood table \n");
-        try{
-            Class.forName("org.sqlite.JDBC");
-            theConnection = DriverManager.getConnection("jdbc:sqlite:foodmood.db");
-            theStatement = theConnection.createStatement();
-            
-            String create = "CREATE TABLE IF NOT EXISTS mood (name varchar, score integer, time integer)";
-            theStatement.executeUpdate(create);
-            
-            
-            
-            theStatement.close();
-            theConnection.close(); 
-        } catch(Exception e){
-            e.printStackTrace();
-            System.exit(0);
-        }
+        
     }
     
     public MoodList(ArrayList<Mood> moods){
@@ -81,37 +63,7 @@ public class MoodList {
      * @param moodProfileToAdd the mood to add
      */
     public void addMoodProfile(Mood moodToAdd) {
-        Connection conn = null;
-        PreparedStatement stmt;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:foodmood.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("Connected to foodmood.db");
-            
-            stmt = conn.prepareStatement("INSERT INTO mood (name, score, time) values (?, ?, ?)");
-            stmt.setString(1, moodToAdd.getName());
-            stmt.setInt(2, moodToAdd.getMoodScore());
-            //stmt.setString(3, foodToAdd.getMoods().toString());
-            stmt.setString(3, moodToAdd.getTime().toString());
-            stmt.executeUpdate();
-            
-            
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                    System.out.println("Closed connection to foodmood.db");
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
+        System.out.println("TODO addMood");
     }
     
     public Mood getMood(int i){
@@ -123,31 +75,7 @@ public class MoodList {
      * @param moodProfileToRemove the mood to remove
      */
     public void removeMoodProfile(Mood moodToRemove) {
-        Connection conn = null;
-        PreparedStatement stmt;
-        try {
-            // db parameters
-            String url = "jdbc:sqlite:foodmood.db";
-            // create a connection to the database
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("Connected to foodmood.db");
-            stmt = conn.prepareStatement("DELETE FROM mood WHERE id = (?)");
-            stmt.setInt(1, moodToRemove.getID());
-            stmt.execute();
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                    System.out.println("Closed connection to foodmood.db");
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
+        System.out.println("TODO removeMood");
     }
     
     /**
