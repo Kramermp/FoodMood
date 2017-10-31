0f30c34d26aa5ec3f8009cd5d1f8b7e68d27ccc6
hkgarthwaite <hkgarthwaite@gmail.com>
2017-10-31 17:49:19 -0400

    Updating comment

--- src/notificationprofile/controller/NotificationCntl.java
+++ src/notificationprofile/controller/NotificationCntl.java
@@ -19,6 +19,11 @@ public class NotificationCntl {
     NotificationList theNotificationList;
     NavigationCntl navigationCntl;
     
+    /**
+     * Default constructor, needs user to get and assign notifications
+     * @param user currentUser
+     * @param navigationCntl 
+     */
     public NotificationCntl(User user, NavigationCntl navigationCntl){
         theUser = user;
         theNotificationList = user.getNotificationList();
@@ -26,11 +31,25 @@ public class NotificationCntl {
         viewNotificationList();
     }
     
+    /**
+     * Deletes notification
+     * @param id 
+     */
+    public void deleteNotification(int id){
+        theNotificationList.deleteNotification(id);
+    }
+    /**
+     * Opens NotificationListUI
+     */
     public void viewNotificationList(){
         NotificationListUI theNotificationListUI = new NotificationListUI(this, theNotificationList);
         theNotificationListUI.setVisible(true);
     }
     
+    /**
+     * Opens NotificationUI for notification
+     * @param id id of the notification to view
+     */
     public void viewNotification(int id){
         Notification theNotificationToView = null;
         for (int i = 0; i < theNotificationList.getNotificationList().size(); i++) {
@@ -44,14 +63,26 @@ public class NotificationCntl {
         theNotificationList.markNotificationAsRead(theNotificationToView.getId());
     }
     
+    /**
+     * sets a notification to read
+     * @param id id of the notification
+     * @return 
+     */
     public boolean notificationIsRead(int id){
         return theNotificationList.getNotificationByID(id).isRead();
     }
     
+    /**
+     * Calls navigationCntl to go home
+     */
     public void goHome(){
         navigationCntl.goHomeScreen();
     }
     
+    /**
+     * Scrolls forward through notificationList
+     * @param id 
+     */
     public void next(int id){
         Notification theNotificationToView = theNotificationList.next(id);
         NotificationUI theNotificationUI = new NotificationUI(this, theNotificationToView);
@@ -59,6 +90,10 @@ public class NotificationCntl {
         theNotificationList.markNotificationAsRead(theNotificationToView.getId());
     }
     
+    /**
+     * Scrolls backwards through notificationList
+     * @param id 
+     */
     public void previous(int id){
         Notification theNotificationToView = theNotificationList.previous(id);
         NotificationUI theNotificationUI = new NotificationUI(this, theNotificationToView);
@@ -66,7 +101,5 @@ public class NotificationCntl {
         theNotificationList.markNotificationAsRead(theNotificationToView.getId());
     }
     
-    public void deleteNotification(int id){
-        theNotificationList.deleteNotification(id);
-    }
+    
 }
